d6e5a93715b1161c38422533e2f153e4e2cc6132
Superdityaa <102494249+Superdityaa@users.noreply.github.com>
2024-11-13 08:52:58 +0700

    style: create font class for design system

---
+++ ostudy/lib/presentation/core/utils/app_textstyles.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ostudy/presentation/core/utils/app_colors.dart';
+
+//Heading Bold
+class HeadingBold {
+  static final TextStyle heading1 = GoogleFonts.oxygen(
+      fontSize: 50, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle heading2 = GoogleFonts.oxygen(
+      fontSize: 42, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle heading3 = GoogleFonts.oxygen(
+      fontSize: 35, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle heading4 = GoogleFonts.oxygen(
+      fontSize: 29, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle heading5 = GoogleFonts.oxygen(
+      fontSize: 24, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle heading6 = GoogleFonts.oxygen(
+      fontSize: 17, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+}
+
+//Heading Regular
+class HeadingRegular {
+  static final TextStyle heading1 = GoogleFonts.oxygen(
+      fontSize: 50, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle heading2 = GoogleFonts.oxygen(
+      fontSize: 42, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle heading3 = GoogleFonts.oxygen(
+      fontSize: 35, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle heading4 = GoogleFonts.oxygen(
+      fontSize: 29, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle heading5 = GoogleFonts.oxygen(
+      fontSize: 24, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle heading6 = GoogleFonts.oxygen(
+      fontSize: 17, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+}
+
+//Paragraph Body
+class ParagraphBody {
+  static final TextStyle mediumBold = GoogleFonts.oxygen(
+      fontSize: 14, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle smallBold = GoogleFonts.oxygen(
+      fontSize: 12, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle xsmallBold = GoogleFonts.oxygen(
+      fontSize: 10, fontWeight: FontWeight.bold, color: NeutralColors.black500);
+  static final TextStyle mediumRegular = GoogleFonts.oxygen(
+      fontSize: 14, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle smallRegular = GoogleFonts.oxygen(
+      fontSize: 12, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+  static final TextStyle xsmallRegular = GoogleFonts.oxygen(
+      fontSize: 10, fontWeight: FontWeight.normal, color: NeutralColors.black500);
+}
