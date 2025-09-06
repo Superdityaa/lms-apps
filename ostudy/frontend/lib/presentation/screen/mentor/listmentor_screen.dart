1f52246bcdf32224362e642c57b4b5fd8e32d260
Superdityaa <aditiyamahendra08@gmail.com>
2025-09-06 18:09:21 +0700

    update: update list mentor screen

--- ostudy/frontend/lib/presentation/screen/mentor/listmentor_screen.dart
+++ ostudy/frontend/lib/presentation/screen/mentor/listmentor_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ostudy/presentation/core/components/avatar.dart';
 import 'package:ostudy/presentation/core/components/page_title.dart';
 
 class ListMentor extends StatefulWidget {
@@ -12,13 +13,31 @@ class _ListMentorState extends State<ListMentor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          child: CustomPageTitle(
-        title: "Our Mentor",
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+        child: Column(
+          children: [
+            CustomPageTitle(
+              title: "Our Mentor",
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            const SizedBox(height: 32),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AvatarProfile(
+                  isEditable: true,
+                ),
+                Column(
+                  children: [],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
